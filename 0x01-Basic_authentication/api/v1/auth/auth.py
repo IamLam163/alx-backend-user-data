b994a1280127a6ec4dfca8935e6bf01548e3342e
IamLam163 <olamidealiyu@gmail.com>
2023-05-02 23:55:52 +0100

    auth validator

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False - path and excluded_paths"""
-        return False
+        # return False
+        if path is None or excluded_paths is None:
+            return True
+        """
+        for exc_path in excluded_paths:
+            if path.startswith(exc_path):
+                if exc_path.endswith('/'):
+                    return False
+                elif path[len(exc_path)] == '/':
+                    return False"""
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object"""
