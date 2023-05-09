5574288d0420223639a71b6244aa8c16a8224d6e
IamLam163 <olamidealiyu@gmail.com>
2023-05-09 23:45:25 +0100

    type checking

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,6 @@ Hashing passwords with bcrypt
 import bcrypt
 
 
-def _hash_password(password):
+def _hash_password(password: str) -> bytes:
     """Hashing passwords with bcrypt"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
