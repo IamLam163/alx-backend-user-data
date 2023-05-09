ce9a8a0b5f528269c52d1cace2aef5e0c6e7f832
IamLam163 <olamidealiyu@gmail.com>
2023-05-09 22:55:46 +0100

    update user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -47,3 +47,13 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """function locates a user object and updates it"""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+        return None
