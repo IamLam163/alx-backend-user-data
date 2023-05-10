1b5940568ca8e352d59d389f26a099ec05f01b4c
IamLam163 <olamidealiyu@gmail.com>
2023-05-10 22:09:13 +0100

    POST new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,17 +2,29 @@
 """
 Basic Flask APP
 """
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def home() -> str:
     """returns a string with a message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def create_user() -> str:
+    """users function that implements the POST /users route."""
+    email, password = (request.form.get(key) for key in ["email", "password"])
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
