dac59a493912ad3f4546edd2012006727f8f8404
IamLam163 <olamidealiyu@gmail.com>
2023-05-11 08:10:58 +0100

    login function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Basic Flask APP
 """
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 from auth import Auth
 
 app = Flask(__name__)
@@ -25,6 +25,18 @@ def create_user() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """login function enables a user to login"""
+    email, password = (request.form.get(key) for key in ["email", "password"])
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    gen_session_id = AUTH.create_session(email)
+    res = jsonify({"email": email, "message": "logged in"})
+    res.set_cookie("session_id", gen_session_id)
+    return res  # res -> response
 
 
 if __name__ == "__main__":
