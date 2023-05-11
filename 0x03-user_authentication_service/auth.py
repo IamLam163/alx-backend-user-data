a2ddb8ce0031eb27d41e3a35141e9971fe72eac6
IamLam163 <olamidealiyu@gmail.com>
2023-05-11 05:54:30 +0100

    validate password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,3 +25,15 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """checks is password matches the hashed password"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                validate_pw = bcrypt.checkpw(password.encode("utf-8"),
+                                             user.hashed_password)
+                return validate_pw
+        except NoResultFound:
+            return False    # if hashed password dies not match
+        return False
