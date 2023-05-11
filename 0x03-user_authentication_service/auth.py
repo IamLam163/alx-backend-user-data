52671ea2f7280baa468de71be38c871600c42b6d
IamLam163 <olamidealiyu@gmail.com>
2023-05-11 06:35:36 +0100

    create session for a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,3 +43,25 @@ class Auth:
         except NoResultFound:
             return False    # if hashed password dies not match
         return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                gen_session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=gen_session_id)
+                return gen_session_id
+            return None
+        except NoResultFound:
+            pass
+
+
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
+
+auth.register_user(email, password)
+
+print(auth.create_session(email))
+print(auth.create_session("unknown@email.com"))
